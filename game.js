7cb62e866576cde42bce7b636d35e73117d86291
dede95 <29232994+dede95@users.noreply.github.com>
2025-05-02 22:28:22 +0100

    Add classes through classList in fucntions for correct sqaure and general sqaures

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 const grid = document.querySelector('#grid');
-const startBtn = document.querySelector('#startGame')
+const startBtn = document.querySelector('#startGame');
+const livesTitle = document.querySelector('#lives');
+const livesNumber = document.querySelector('#lives span');
 
 let currentLevel = 1;
+let lives = 3;
 let gameOver = false;
 const gridnbyn = {};
 
@@ -13,28 +16,30 @@ const makeBaseColor = () => {
     return [rVal, gVal, bVal, `rgb(${rVal}, ${gVal}, ${bVal})`]
 }
 
-const randomSelectSquare = (gridObj) => {
-    let childrenSquares = gridObj.children;
-    let chosenSqaure = childrenSquares[Math.floor(Math.random() * childrenSquares.length)];
-
-    return chosenSqaure 
-}
-
 const generateGrid = (level) => {
     let sqaures = (level + 1) ** 2;
     for(let i = 0; i < sqaures; i++){
         let sqaure = document.createElement('div');
-        sqaure.classList.add('grid-item');
+        sqaure.classList.add('grid-item', 'sqaure');
         grid.appendChild(sqaure);
     }
     grid.style.gridTemplateColumns = `repeat(${level + 1}, 100px [col-start])`;
+}
+
+const randomSelectSquare = (gridObj) => {
+    let childrenSquares = gridObj.children;
+    let chosenSqaure = childrenSquares[Math.floor(Math.random() * childrenSquares.length)];
+
+    chosenSqaure.classList.toggle('sqaure') //switch off square for the correct sqaure
+    chosenSqaure.classList.add('correct');
+    return chosenSqaure 
 }
 
 const adjustColour = (colour) => {
     let adjustedColour;
 
     // range -4 to 4 
-    let adjusters = [-10, -7, -5, -4, 4, 5, 7, 10];
+    let adjusters = [-10, -7, -5, 5, 7, 10];
     let adjuster = adjusters[Math.floor(Math.random() * adjusters.length)]; 
 
     // grab only the rgb values from the original base colour
@@ -49,7 +54,12 @@ const adjustColour = (colour) => {
 
 }
 
+// function gamePlay() {
+
+// }
+
 // EVENT LISTENERS
+livesTitle.style.display = 'none'
 
 startBtn.addEventListener('click', function () {
     this.style.display = 'none';
@@ -67,6 +77,17 @@ startBtn.addEventListener('click', function () {
 
     let randomSqaure = randomSelectSquare(grid); 
     randomSqaure.style.backgroundColor = adjustColour(chosenColorArray);
+
+    // randomSqaure.addEventListener('click', function(e){
+    //     e.stopPropagation();
+    //     console.log(this.style.backgroundColor);
+    // })
+
+    livesTitle.style.display = 'inline-block'
+    livesNumber.innerHTML = `${lives}`;
+    
+
+    
 });
 
 
